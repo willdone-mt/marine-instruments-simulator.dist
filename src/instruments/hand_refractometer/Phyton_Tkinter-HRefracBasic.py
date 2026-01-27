0d324d64f082067218311428de7d0aa78f0551bd
WillDone-MT <wildanmufti350@gmail.com>
2026-01-27 21:33:42 +0700

    InDev Preparation [5] - Handrefrac with TTK

--- src/instruments/hand_refractometer/Phyton_Tkinter-HRefracBasic.py
+++ src/instruments/hand_refractometer/Phyton_Tkinter-HRefracBasic.py
@@ -1,14 +1,13 @@
 import tkinter as tk
+from tkinter import ttk
 import value_generator as vg
 
-def greet():
-    label.config(text=f"Hello, {entry.get()}!")
-
+# Can we make this def as a external modukle file? 
 def peek():
     label2.config(text=f"You got the number {vg.random_value()}")
 
 def calibration():
-    label2.config(text=f"Refractomater calibrated succesfully! value returned to 0 !")
+    label2.config(text=f"Refractomater calibrated succesfully! \nvalue returned to 0 !")
 
 # Windows
 root = tk.Tk()
@@ -16,34 +15,28 @@ root = tk.Tk()
 # Window Title
 root.title("HandRefractometer Tkinter Example")
 
+# ttk Style
+style = ttk.Style()
+style.configure("BW.TLabel", foreground="black", background="white")
+
 # Texts
-label = tk.Label(root, text="Enter your name:")
-label2 = tk.Label(root, text="What Value do you got?")
+label2 = ttk.Label(root, text="What Value do you got?")
 
 # Lay out the Label in the window using the "pack" geometry manager.
 # - pack() places the widget sequentially (top to bottom by default).
 # - Tkinter has three geometry managers: pack, grid, and place.
-label.pack()
 label2.pack(side='right')
 
-# Create an Entry widget to accept user input (single line text field).
-# - Parent is "root".
-entry = tk.Entry(root)
-
-# Lay out the Entry below the Label (because pack stacks them in creation order).
-entry.pack(after=label)
-
 # Buttons.
-button = tk.Button(root, text="Greet Me", command=greet)
 
-button2 = tk.Button(root, text="Peek", command=peek)
+button2 = ttk.Button(root, text="Peek", command=peek)
 
-button3 = tk.Button(root, text="Calibrate", command=calibration)
+button3 = ttk.Button(root, text="Calibrate", command=calibration)
 
 # Button Layouts
-button.pack()
-button2.pack()
 button3.pack()
+button2.pack()
+
 
 # Keeps the window responsive until the user closes it or the program exits.
 root.mainloop()
